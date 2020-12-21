77075adad582e8d3ace0d57eb4751ce2ac6ce869
sekonioluwaseun@gmail.com <sekonioluwaseun@gmail.com>
2020-12-21 13:38:56 +0100

    Implemented image ranking with redis

--- images/views.py
+++ images/views.py
@@ -71,7 +71,12 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # increment total image views by 1 on redis DB
+    # store in redis like 'image:2:views and return the value of the key image:2:views
+    # The incr() method returns the final value of the key after performing the operation.
     total_views = r.incr(f'image:{image.id}:views')
+    # print(total_views)
+    # increment image ranking by 1
+    r.zincrby('image_ranking', 1, image.id)
     return render(request, 'images/image/detail.html', {'section': 'images', 'image': image, 'total_views':total_views})
 
 @ajax_required
@@ -96,3 +101,14 @@ def image_like(request):
         except:
             pass
     return JsonResponse({'error': 'error'})
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary      
+    image_ranking = r.zrange('image_ranking', 0, -1,
+    desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,'images/image/ranking.html',{'section': 'ranking','most_viewed': most_viewed})
